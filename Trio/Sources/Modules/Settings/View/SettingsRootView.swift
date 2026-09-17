f3f279d35a0d665152055b961cae76eaa4a009d8
Mike Plante <82073483+MikePlante1@users.noreply.github.com>
2026-09-17 12:10:36 -0400

    Merge pull request #1541 from MikePlante1/release-notes-button
    Rename "Previous Releases" button to "Release Notes"
    
    (cherry picked from commit 43100b3421fb062c24396d841ed916cfd8b2c967)

--- Trio/Sources/Modules/Settings/View/SettingsRootView.swift
+++ Trio/Sources/Modules/Settings/View/SettingsRootView.swift
@@ -414,7 +414,7 @@ extension Settings {
                                     NavigationLink(
                                         destination: ReleaseNotesListView(releases: releaseNotesService.previousReleases)
                                     ) {
-                                        Text("Previous Versions")
+                                        Text("Release Notes")
                                             .foregroundColor(.primary)
                                     }
                                 }
